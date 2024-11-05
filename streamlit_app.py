3674ab8aedffde4d7f28723b00fe3ee76096c6b1
VelezJS <mvelezsuarez04@gmail.com>
2024-11-05 16:39:32 -0500

    Actualizar la ruta de credenciales

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from datetime import datetime
 
 # Configuración de autenticación de Google Sheets
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('.streamlit/credentials.json', scope)
 client = gspread.authorize(creds)
 
 try:
